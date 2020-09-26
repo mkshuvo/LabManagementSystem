72cabc964dcb38da2ed85c34ea911bc7f8fddcd8
mkshuvo <mkshuvo@hotmail.com>
2020-09-27 01:07:30 +0600

    first commit

---
+++ LabManagementSystem.Shared/LoginViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabManagementSystem.Shared
+{
+    class LoginViewModel
+    {
+    }
+}
---
+++ LabManagementSystem/Models/LabEquipmentRelation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LabManagementSystem.Models
+{
+    public class LabEquipmentRelation
+    {
+    }
+}
